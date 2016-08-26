c39a5833246ea99f404c4616bbd609c3cbf751dc
samidrawat@gmail.com <samidrawat@gmail.com>
2016-08-26 13:39:23 -0700

    my first commit

---
+++ SPA1/Controllers/AllControllers.js
@@ -0,0 +1,12 @@
+﻿app.controller('myController', function ($scope) {
+    $scope.message = 'Hi, This is Home page!!!';
+});
+app.controller('AboutController', function ($scope) {
+    $scope.message = 'Hi, This is About page!!!';
+});
+app.controller('ContactController', function ($scope) {
+    $scope.message = 'Hi, This is Contact page!!!';
+});
+app.controller('TestController', function ($scope) {
+    $scope.message = 'Hi, This is Test page!!!';
+});
---
+++ SPA1/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('myApp',['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'tpl/Home.html',
+        controller: 'myController'
+    })
+    .when('/about', {
+        templateUrl: 'tpl/About.html',
+        controller: 'AboutController'
+    })
+    .when('/contact', {
+        templateUrl: 'tpl/Contact.html',
+        controller: 'ContactController'
+    })
+    .when('/test', {
+        templateUrl: 'tpl/Test.html',
+        controller: 'TestController'
+    });
+}]);
